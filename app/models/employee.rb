0de18ed483effd66794ba8e578587789f87fc2d4
github username <github email address>
2019-06-12 13:44:04 -0700

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,18 @@
+class MyValidation < ActiveModel::Validator
+    def validate(record)
+        aliases = Employee.all.collect do |emp|
+            emp.alias
+        end
+        titles = Employee.all.collect do |emp|
+            emp.title
+        end
+        if aliases.include?(record[:alias]) || titles.include?(record[:title])
+            record.errors[:name] << "Dwights Rule"
+        end
+    end
+end
+
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates_with MyValidation
 end
