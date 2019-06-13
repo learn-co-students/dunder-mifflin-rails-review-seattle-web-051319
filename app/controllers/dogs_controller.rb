695b18a8b541212abbf233cb76ffe2ced0bf2bba
Bluesbaka <badunnells@gmail.com>
2019-06-13 10:24:49 -0700

    navigated by Austin and co, driven by Bryn

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,50 @@
 class DogsController < ApplicationController
+ before_action :get_params, only:[:show, :edit, :update]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  @employees = @dog.employees
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to @dog
+    else
+      render :new
+    end
+  end
+
+    def edit
+    end
+
+    def update
+      if @dog.update(dog_params)
+        redirect_to @dog
+      else
+        render :edit
+      end
+    end
+
+
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
+  def get_params
+    @dog = Dog.find(params[:id])
+  end
+
+
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+ before_action :get_params, only:[:show, :edit, :update]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+    def edit
+    end
+
+    def update
+      if @employee.update(employee_params)
+        redirect_to @employee
+      else
+        render :edit
+      end
+    end
+
+
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
+  def get_params
+    @employee = Employee.find(params[:id])
+  end
+
+
+
 end
