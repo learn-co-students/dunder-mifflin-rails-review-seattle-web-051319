872cfad14165a1abb4be06d5ee4d027f36b95edf
AustinBH <harlowaustin2@gmail.com>
2019-06-13 10:46:23 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    post '/dogs/sort', to: 'dogs#sort', as: 'sort'
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
