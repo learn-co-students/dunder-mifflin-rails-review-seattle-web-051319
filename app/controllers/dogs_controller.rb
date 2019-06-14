9ed2b63d3dc1ea1546532ce6382628b8ba667483
fut0k0 <cccabell@icloud.com>
2019-06-14 09:13:05 -0700

    Completed lab

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+	
+	def index
+		@dogs = Dog.all.sorted
+	end
+	
+	def show
+		@dog = Dog.find(params[:id])
+	end
+		
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+	has_many :employees
+	
+	def self.sorted
+		self.all.sort_by {|dog| dog.employees.size} .reverse
+	end
+	
 end
